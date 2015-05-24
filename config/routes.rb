993853f9b50daa7189e58ebf32e043f507348b54
Todd Squitieri <vagrant@precise32.(none)>
2015-05-24 03:14:35 +0000

    I set up the lessons root path in the config folder.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Memory::Application.routes.draw do
+root 'lessons#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
